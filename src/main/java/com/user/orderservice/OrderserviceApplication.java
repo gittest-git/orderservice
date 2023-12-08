1374fb5e268189a95ba729dc96573fcbc63b5fae
Siva kumar Ch <siva@Sivas-Air.attlocal.net>
2023-12-07 22:13:17 -0600

    order service code changes

---
+++ src/main/java/com/user/orderservice/OrderserviceApplication.java
@@ -0,0 +1,30 @@
+package com.user.orderservice;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class OrderserviceApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(OrderserviceApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application OrderService Started....");
+	}
+
+	@GetMapping("/getOrders")
+	public String getOrders()
+	{
+		return "All ordered items";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+
+}
