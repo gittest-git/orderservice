18b48de1e96a89fb3f17b7bcbff30cbfe68cfc31
Siva kumar Ch <siva@Sivas-Air.attlocal.net>
2023-12-11 23:36:30 -0600

    docker file changes

--- src/main/java/com/user/orderservice/OrderserviceApplication.java
+++ src/main/java/com/user/orderservice/OrderserviceApplication.java
@@ -6,8 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OrderserviceApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(OrderserviceApplication.class);
